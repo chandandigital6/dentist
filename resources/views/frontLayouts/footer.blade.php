9a1c9095101954de1ec015e1060e3d68f3590f2f
chandan <chandan84yadav@gmail.com>
2024-05-20 11:04:33 +0530

    navbar active

--- resources/views/frontLayouts/footer.blade.php
+++ resources/views/frontLayouts/footer.blade.php
@@ -46,7 +46,7 @@
                 <p class="mb-md-0">&copy; <a class="text-white border-bottom" href="#">Your Site Name</a>. All Rights Reserved.</p>
             </div>
             <div class="col-md-6 text-center text-md-end">
-                <p class="mb-0">Designed by <a class="text-white border-bottom" href="https://htmlcodex.com">HTML Codex</a></p>
+                <p class="mb-0">Designed by <a class="text-white border-bottom" href="http://realvictorygroups.com/">Real Victory Groups</a></p>
             </div>
         </div>
     </div>
--- resources/views/frontLayouts/header.blade.php
+++ resources/views/frontLayouts/header.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg bg-white navbar-light shadow-sm px-5 py-3 py-lg-0">
-    <a href="index.html" class="navbar-brand p-0">
+    <a href="{{ route('home') }}" class="navbar-brand p-0">
         <h1 class="m-0 text-primary"><i class="fa fa-tooth me-2"></i>DentCare</h1>
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
@@ -7,21 +7,20 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
         <div class="navbar-nav ms-auto py-0">
-            <a href="{{route('home')}}" class="nav-item nav-link active">Home</a>
-            <a href="{{route('about')}}" class="nav-item nav-link">About</a>
-            <a href="{{route('services')}}" class="nav-item nav-link">Service</a>
+            <a href="{{ route('home') }}" class="nav-item nav-link {{ request()->routeIs('home') ? 'active' : '' }}">Home</a>
+            <a href="{{ route('about') }}" class="nav-item nav-link {{ request()->routeIs('about') ? 'active' : '' }}">About</a>
+            <a href="{{ route('services') }}" class="nav-item nav-link {{ request()->routeIs('services') ? 'active' : '' }}">Service</a>
             <div class="nav-item dropdown">
-                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                <a href="#" class="nav-link dropdown-toggle {{ request()->routeIs(['price', 'team', 'testimonial', 'appointment']) ? 'active' : '' }}" data-bs-toggle="dropdown">Pages</a>
                 <div class="dropdown-menu m-0">
-                    <a href="{{route('price')}}" class="dropdown-item">Pricing Plan</a>
-                    <a href="{{route('team')}}" class="dropdown-item">Our Dentist</a>
-                    <a href="{{route('testimonial')}}" class="dropdown-item">Testimonial</a>
-                    <a href="{{route('appointment')}}" class="dropdown-item">Appointment</a>
+                    <a href="{{ route('price') }}" class="dropdown-item {{ request()->routeIs('price') ? 'active' : '' }}">Pricing Plan</a>
+                    <a href="{{ route('team') }}" class="dropdown-item {{ request()->routeIs('team') ? 'active' : '' }}">Our Dentist</a>
+                    <a href="{{ route('testimonial') }}" class="dropdown-item {{ request()->routeIs('testimonial') ? 'active' : '' }}">Testimonial</a>
+                    <a href="{{ route('appointment') }}" class="dropdown-item {{ request()->routeIs('appointment') ? 'active' : '' }}">Appointment</a>
                 </div>
             </div>
-            <a href="{{route('contact')}}" class="nav-item nav-link">Contact</a>
+            <a href="{{ route('contact') }}" class="nav-item nav-link {{ request()->routeIs('contact') ? 'active' : '' }}">Contact</a>
         </div>
-        <button type="button" class="btn text-dark" data-bs-toggle="modal" data-bs-target="#searchModal"><i class="fa fa-search"></i></button>
-        <a href="{{route('appointment')}}" class="btn btn-primary py-2 px-4 ms-3">Appointment</a>
+        <a href="{{ route('appointment') }}" class="btn btn-primary py-2 px-4 ms-3">Appointment</a>
     </div>
 </nav>
